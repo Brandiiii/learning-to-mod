e809d5ab5404f88d26e20f02bdca9a532e85346f
Brandon O'Connell <brandonoconnell37@gmail.com>
2023-03-16 11:47:13 +0000

    Initial commit

---
+++ src/main/java/dev/brandonoconnell/demomod/DemoMod.java
@@ -0,0 +1,35 @@
+package dev.brandonoconnell.demomod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(DemoMod.MOD_ID)
+public class DemoMod {
+    public static final String MOD_ID = "demomod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public DemoMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
